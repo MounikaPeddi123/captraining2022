d22071977e440dd946ee9619e0f52794db8ecb85
MounikaPeddi123 <mounikap0805@gmail.com>
2022-03-03 22:38:52 +0530

    mocktest

---
+++ mocktest/src/mocktest/StringOccurance.java
@@ -0,0 +1,24 @@
+package mocktest;
+
+
+import java.util.Scanner;
+
+public class StringOccurance{
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String a=sc.next();
+		char b='g';
+		int count=1;
+		for(int i=0;i<a.length();i++)
+		{
+			if(a.charAt(i)==b)
+			{
+				count=count+1;
+			}
+		}
+		System.out.println(count);
+		
+	}
+
+}
